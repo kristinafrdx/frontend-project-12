6aaa0bfa2eb27520b9b303049351115d096bfb80
kristinafrdx <kristina.frdx@mail.ru>
2024-05-15 21:34:18 +0400

    fix

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -66,6 +66,7 @@ const Registration = () => {
             if (response.data.token) {
               localStorage.setItem('username', response.data.username);
               localStorage.setItem('token', response.data.token);
+              sessionStorage.setItem('token', response.data.token);
               addToken(response.data.token);
               addUserName(response.data.username);
               navigate('/');
